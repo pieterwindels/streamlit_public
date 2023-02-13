a8987c912a8d698f8c002f5d0470edb58e59d1f1
pieterwindels <pieter.windels@telenet.be>
2023-02-13 11:59:51 +0100

    Update pricecloud_public.py

--- pricecloud_public.py
+++ pricecloud_public.py
@@ -42,7 +42,7 @@ def url_constructor (koop_huur, pand, hoofdgemeente):
 
   #import the postcodes dictionary from pickle file:
 
-  with open('postcodes.pickle', 'rb') as f:
+  with open('postcodes_public.pickle', 'rb') as f:
     postcodes=pickle.load(f)
   
   #define the URL part (as string) to reflect the hoofdgemeente (we use the postcodes dictionary):
@@ -495,7 +495,7 @@ if st.session_state.count==0:
     with st.form('invulformulier'):
       st.header('Van welke panden wens je een up-to-date lijst?')
       pand=st.radio('Wat is het type vastgoed?', ['huis', 'appartement', 'garage', 'bedrijfsvastgoed', 'grond'], key='pand')
-      hoofdgemeente=st.selectbox('In welke gemeente is het pand gelegen?', list(set(postcodes.values())), key='hoofdgemeente',
+      hoofdgemeente=st.selectbox('In welke gemeente is het pand gelegen?', list(set(postcodes_public.values())), key='hoofdgemeente',
                                 help='In this trial version only a few options can be selected,/n in the paid version all options are visible.')
       koop_huur=st.radio('Te koop of te huur?', ['te-koop', 'te-huur'], key='koop_huur')
       st.form_submit_button(label="Geef me de lijst", on_click=increment_counter)
