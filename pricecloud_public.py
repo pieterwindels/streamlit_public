57ad1811e6fde79ab4ba63a6f1616a231d28c146
pieterwindels <pieter.windels@telenet.be>
2023-02-14 10:19:46 +0100

    Update pricecloud_public.py

--- pricecloud_public.py
+++ pricecloud_public.py
@@ -60,27 +60,6 @@ def increment_counter():
 def counter_reset():
     st.session_state.count=0
     
-#FUNCTION TO CONSTRUCT SEARCH URL
-@st.cache(ttl=86400, show_spinner=False)
-def url_constructor (koop_huur, pand, hoofdgemeente):
-
-    #define the URL part to reflect the pand type:
-
-  if pand=='bedrijfsvastgoed':
-    pand_zi, pand_iw=pand,'handelszaak'
-  else:
-    pand_zi, pand_iw=pand, pand
-
-  #define the URL part (as string) to reflect the hoofdgemeente (we use the postcodes dictionary):
-
-  hoofdgemeente_iw=''.join([k+',' for k,v in postcodes.items() if v==hoofdgemeente]).strip(',')
-  
-  #construct the full length URL:
-
-  url_zi='https://www.zimmo.be/nl/'+hoofdgemeente+'/'+koop_huur+'/'+pand_zi+'/#gallery'
-  url_iw='https://www.immoweb.be/nl/zoeken/'+pand_iw+'/'+koop_huur+'?countries=BE&postalCodes='+hoofdgemeente_iw+'&page=1&orderBy=relevance'
-  
-  return url_zi, url_iw
 
 #FUNCTION TO SCRAPE THE SELECTED ZM PAGES
 @st.cache(ttl=86400, show_spinner=False)
