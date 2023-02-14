eb662a42ebbcb71922327ee9512d6111261414ff
pieterwindels <pieter.windels@telenet.be>
2023-02-14 12:35:45 +0100

    Update scrape_iw.py

--- scrape_iw.py
+++ scrape_iw.py
@@ -30,29 +30,33 @@ def scrape_iw(url_iw):
     text=page.text
     #we collect the number of pages results
     aantal_pg=text.partition(":result-count='")[2].partition("'")[0]
-    aantal_pg=-(-(int(aantal_pg))//30)
-    #we collect the webpage 1 we want to scrape
-    a=text.partition(":results='")[2].partition(":results-storage='")[0].rstrip()
-    b=a.replace('&quot;','"').replace("[]", "null").rstrip("'")
-    vastgoed_data_json=json.loads(b, strict=False)
-    vastgoed_data.append(vastgoed_data_json)
-    #we collect all the following pages
-    while ct<=int(aantal_pg):
-      try:
-        time.sleep(random.randint(2,5))
-        url=url_iw.replace('1&orderBy=relevance', str(ct)+'&orderBy=relevance')
-        page = requests.get(url, headers = headers)
-        text=page.text
-        a=text.partition(":results='")[2].partition(":results-storage='")[0].rstrip()
-        b=a.replace('&quot;','"').replace("[]", "null").rstrip("'")
-        vastgoed_data_json=json.loads(b, strict=False)
-        vastgoed_data.append(vastgoed_data_json)
-        ct=ct+1
-        if (ct==8 or ct==15 or ct==23 or ct==32):
-            time.sleep(10) 
-      except Exception as e:
-        df_IW=pd.DataFrame({})
-        return df_IW
+    if int(aantal_pg)==0:
+      df_IW=pd.DataFrame({})
+      return df_IW
+    else:
+      aantal_pg=-(-(int(aantal_pg))//30)
+      #we collect the webpage 1 we want to scrape
+      a=text.partition(":results='")[2].partition(":results-storage='")[0].rstrip()
+      b=a.replace('&quot;','"').replace("[]", "null").rstrip("'")
+      vastgoed_data_json=json.loads(b, strict=False)
+      vastgoed_data.append(vastgoed_data_json)
+      #we collect all the following pages
+      while ct<=int(aantal_pg):
+        try:
+          time.sleep(random.randint(2,5))
+          url=url_iw.replace('1&orderBy=relevance', str(ct)+'&orderBy=relevance')
+          page = requests.get(url, headers = headers)
+          text=page.text
+          a=text.partition(":results='")[2].partition(":results-storage='")[0].rstrip()
+          b=a.replace('&quot;','"').replace("[]", "null").rstrip("'")
+          vastgoed_data_json=json.loads(b, strict=False)
+          vastgoed_data.append(vastgoed_data_json)
+          ct=ct+1
+          if (ct==8 or ct==15 or ct==23 or ct==32):
+              time.sleep(10) 
+        except Exception as e:
+          df_IW=pd.DataFrame({})
+          return df_IW
   except Exception as e:
     df_IW=pd.DataFrame({})
     return df_IW
