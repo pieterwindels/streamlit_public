e7db4adf7b4e4ada665180d6d028c51db6afc653
pieterwindels <pieter.windels@telenet.be>
2023-02-14 10:26:12 +0100

    Update pricecloud_public.py

--- pricecloud_public.py
+++ pricecloud_public.py
@@ -30,16 +30,6 @@ def counter_reset():
     st.session_state.count=0
     
 
-#FUNCTION TO COMBINE THE SCRAPED IW PROPERTIES AND SCRAPED PRIVATE PHONE NRS
-def df_IW_concat (df_IW, df_IW_contact):
-  try:
-    df_IW=pd.merge(df_IW, df_IW_contact, how='outer', on='id')
-  except Exception as e:
-    df_IW=pd.DataFrame({})
-    return df_IW
-    
-  return df_IW
-
 #FUNCTION TO DELETE COLUMNS NOT NEEDED IN SCRAPED DFS
 def delete_columns_rows(df_ZM, df_IW):
   
