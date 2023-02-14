f7c3502df31ed1a67b6b93b231377aac26145785
pieterwindels <pieter.windels@telenet.be>
2023-02-14 10:28:14 +0100

    Update pricecloud_public.py

--- pricecloud_public.py
+++ pricecloud_public.py
@@ -30,29 +30,6 @@ def counter_reset():
     st.session_state.count=0
     
 
-#FUNCTION TO RENAME THE COLUMNS IN SCRAPED DFS
-def rename_columns(df_ZM, df_IW):
-  
-  #define the dict listing all the columns that need to be renamed:
-
-  rename_zi={'particulier_pand_id':'immo_privaat_zimmo', 'b_woonopp':'woonopp', 'address':'adres',
-             'energyLabel':'energielabel', 'advertiser-name':'adverteerder', 'sticker':'extra_info',
-             'price_drop_date':'prijs_verlaagd', 'tijd_sinds_publ':'dagen_online'}
-  rename_iw={'customerName':'adverteerder', 'property-bedroomCount':'slaapkamers', 
-             'property-location-locality':'gemeente',
-             'property-location-postalCode':'postcode', 'property-location-street':'adres',
-             'property-location-number':'huisnr', 'property-location-latitude':'lat',
-             'property-location-longitude':'lon', 'property-netHabitableSurface':'woonopp',
-             'property-landSurface':'perceel_opp', 'price-mainValue':'prijs', 
-             'price-additionalValue':'prijs_extra_kosten', 'price-oldValue':'oude_prijs'}
-
-  
-  if not df_ZM.empty:
-    df_ZM.rename(columns=rename_zi, inplace=True)
-  if not df_IW.empty:
-    df_IW.rename(columns=rename_iw, inplace=True)
-  
-  return df_ZM, df_IW
 
 #FUNCTION TO CLEAN THE DATA IN THE ZM DF
 def clean_df_ZM(df_ZM, koop_huur, pand):
