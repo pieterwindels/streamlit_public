72d5c3bf578ee852ff5727ae8cbd72272a231085
pieterwindels <pieter.windels@telenet.be>
2023-02-14 16:38:31 +0100

    Update pricecloud_public.py

--- pricecloud_public.py
+++ pricecloud_public.py
@@ -114,7 +114,7 @@ if st.session_state.count==1:
         
         st._legacy_dataframe(g_fin.style
                              .format('€{:.0f}', subset=['prijs_m2', 'prijs', 'prijs_extra_kosten', 'oude_prijs'])
-                             .format('{:.0f}', subset=['woonopp']))
+                             .format('{:.0f}m²', subset=['woonopp']))
         
         #prepare the Excel file for download:
         output = BytesIO()
