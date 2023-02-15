211c305fe86922b1c7d02abdf55a5e2390979e81
pieterwindels <pieter.windels@telenet.be>
2023-02-15 11:59:30 +0100

    Update scrape_zi.py

--- scrape_zi.py
+++ scrape_zi.py
@@ -18,7 +18,7 @@ def scrape_zi(url_zi):
   are cached for 24h.
   """
   #define scrape headers:
-  headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36',
+  headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Encoding':'gzip, deflate, br',
             'Accept-Language':'en-US,en;q=0.9',
